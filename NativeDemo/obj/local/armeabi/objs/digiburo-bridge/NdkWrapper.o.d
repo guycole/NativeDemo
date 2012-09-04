af3ff04b4bb066685705e214861c70dc078ca1b2
Guy Cole <guycole@gmail.com>
2012-09-04 14:58:30 -0700

    updated readme

---
+++ NativeDemo/obj/local/armeabi/objs/digiburo-bridge/NdkWrapper.o.d
@@ -0,0 +1,26 @@
+/home/gsc/git/NativeDemo/NativeDemo/obj/local/armeabi/objs/digiburo-bridge/NdkWrapper.o: \
+ /home/gsc/git/NativeDemo/NativeDemo/jni/NdkWrapper.cpp \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /home/gsc/git/NativeDemo/NativeDemo/jni/com_digiburo_example_native_demo_NdkWrapper.h
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/gsc/android/android-ndk-r8b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/home/gsc/git/NativeDemo/NativeDemo/jni/com_digiburo_example_native_demo_NdkWrapper.h:
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ NativeDemo
 ==========
 
 Android NDK Demonstration 
+
+http://guycole.blogspot.com/2012/03/yet-another-android-ndk-blog-posting.html
